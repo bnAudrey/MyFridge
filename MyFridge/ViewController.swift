16eb037ff60815d2926ade67643bd0fcf6f3443f
bnAudrey <brigitta.nataudrey@gmail.com>
2022-04-27 08:37:20 +0700

    Initial Commit

---
+++ MyFridge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFridge
+//
+//  Created by Brigitta Audrey on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
